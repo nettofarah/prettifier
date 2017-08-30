78d544995f59f208c4bd3f17ae6ad53fa4d44e6e
Netto Farah <nettofarah@gmail.com>
2017-08-30 11:40:49 -0700

    Try out very barebones setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Prettifier
+
+
+## Packages
+
+- [lint-staged](https://github.com/okonet/lint-staged) - Runs linters against staged git files
+- [prettier](https://github.com/prettier/prettier) - Prettier
+- [husky](https://github.com/typicode/husky) - Git hooks made easy
---
+++ index.js
@@ -0,0 +1,29 @@
+const { find, exec } = require('shelljs')
+const { writeAt } = require('json-crate')
+
+const usingYarn = find('./yarn.lock')[0].includes('yarn.lock')
+const packageManager = usingYarn ? 'yarn' : 'npm'
+
+const yarnPrefix = 'yarn add --dev'
+const npmPrefix  =  'npm install --dev'
+
+const installPrefix = usingYarn ? yarnPrefix : npmPrefix
+
+// TODO: check for packages?
+
+console.log('Installing prettier')
+      exec(`${installPrefix} prettier`)
+
+console.log('Installing lint-staged')
+exec(`${installPrefix} lint-staged`)
+
+console.log('Installing husky')
+exec(`${installPrefix} husky`)
+
+writeAt('./package.json', 'scripts.precommit', 'lint-staged')
+writeAt('./package.json', 'lint-staged', {
+  '*.js': [
+    'prettier --write --single-quote --no-semi',
+    'git add'
+  ]
+})
