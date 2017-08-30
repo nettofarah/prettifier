c5ce5da156a12e9b649c887159fb2dbe801d1ecc
Netto Farah <nettofarah@gmail.com>
2017-08-30 11:50:05 -0700

    Write files in the correct order

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const usingYarn = find('./yarn.lock')[0].includes('yarn.lock')
 const packageManager = usingYarn ? 'yarn' : 'npm'
 
 const yarnPrefix = 'yarn add --dev'
-const npmPrefix  =  'npm install --dev'
+const npmPrefix = 'npm install --dev'
 
 const installPrefix = usingYarn ? yarnPrefix : npmPrefix
 
 // TODO: check for packages?
 
 console.log('Installing prettier')
-      exec(`${installPrefix} prettier`)
+exec(`${installPrefix} prettier`)
 
 console.log('Installing lint-staged')
 exec(`${installPrefix} lint-staged`)
@@ -20,10 +20,10 @@ exec(`${installPrefix} lint-staged`)
 console.log('Installing husky')
 exec(`${installPrefix} husky`)
 
-writeAt('./package.json', 'scripts.precommit', 'lint-staged')
-writeAt('./package.json', 'lint-staged', {
-  '*.js': [
-    'prettier --write --single-quote --no-semi',
-    'git add'
-  ]
+const lintStageConfig = writeAt('./package.json', 'lint-staged', {
+  '*.js': ['prettier --write --single-quote --no-semi', 'git add']
+})
+
+lintStageConfig.then(() => {
+  writeAt('./package.json', 'scripts.precommit', 'lint-staged')
 })
